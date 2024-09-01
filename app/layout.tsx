ed8dfabf227d6229bfbe9c41896b0365ccfc2146
MrNefi <shwiftymr5@gmail.com>
2024-08-31 22:12:03 -0600

    Fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,35 @@
+import type { Metadata } from "next";
 import "./globals.css";
 import AppWalletProvider from "./components/AppWalletProvider";
 import { NavBar } from "./components/NavBar";
 import Head from 'next/head'; // Import Head from next/head
 
+export const metadata: Metadata = {
+	title: "BORED PITTY KENNEL CLUB",
+	description:
+	  "5,000 Unique Digital Collectibles Living on the Solana Blockchain",
+	openGraph: {
+	  title: "Monster Verse Mint",
+	  description:
+		"5,000 Unique Digital Collectibles Living on the Solana Blockchain",
+	  url: "https://boredpitty.vip/",
+	  siteName: "BORED PITTY KENNEL CLUB",
+	  images: [
+		{
+		  url: "/bpkc.jpg",
+		  width: 1260,
+		  height: 800,
+		},
+	  ],
+	},
+	twitter: {
+	  site: `@MisterMoonOG`,
+	  creator: `@MisterMoonOG`,
+	  card: "summary",
+	},
+	category: "blockchain",
+	icons: "/bpkc.jpg",
+  };
 
 export default function RootLayout({
 	children,
@@ -11,6 +38,7 @@ export default function RootLayout({
   }>) {
 	return (
 	  <html lang="en" >
+
 	  <Head>
         <title>Bored Pitty Kennel Club</title> {/* Set your desired title here */}
         <link rel="icon" href="/public/bpkc.jpg" /> {/* Update the path to your favicon */}
